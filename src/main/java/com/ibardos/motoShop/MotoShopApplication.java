5a9f40dd5369ad79e8b1eb39f38ca450c5c9457c
Bárdos István <bardos.istvan@icloud.com>
2023-01-08 15:08:48 +0100

    Initial commit

---
+++ src/main/java/com/ibardos/motoShop/MotoShopApplication.java
@@ -0,0 +1,13 @@
+package com.ibardos.motoShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotoShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ibardos/motoShop/MotoShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibardos.motoShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotoShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
