cdf59a8ae3c0268d5c265fba9560036283b40aa6
Bárdos István <bardos.istvan@icloud.com>
2023-01-29 11:32:06 +0100

    Create services page at Motorcycles root
    Clickable cards for navigation among available Motorcycle related services

--- src/main/ui/src/pages/motorcycles/Motorcycles.js
+++ src/main/ui/src/pages/motorcycles/Motorcycles.js
@@ -1,9 +1,48 @@
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+
+import motorcycleModelImage from "../../assets/img/motorcycles/motorcycleModel.jpg";
+import motorcycleStockImage from "../../assets/img/motorcycles/motorcycleStock.jpg";
+import {Link} from "react-router-dom";
+
 const Motorcycles = () => {
     return (
         <>
             <h2 className="pageTitle">Motorcycles</h2>
+            <Services />
         </>
     )
 }
+
+const Services = () => {
+    return (
+        <Row xs={1} md={2} className="g-3">
+            <Col>
+                <Card bg={"dark"}>
+                    <Card.Body>
+                        <Card.Title>Motorcycle model</Card.Title>
+                        <Card.Text>
+                            Create, read, update, delete Motorcycle models.
+                        </Card.Text>
+                    </Card.Body>
+                    <Link as={Link} to="/motorcycles/model"><Card.Img variant="bottom" src={motorcycleModelImage} /></Link>
+                </Card>
+            </Col>
+
+            <Col>
+                <Card bg={"dark"}>
+                    <Card.Body>
+                        <Card.Title>Motorcycle stock</Card.Title>
+                        <Card.Text>
+                            Create, read, update, delete Motorcycle models in stock.
+                        </Card.Text>
+                    </Card.Body>
+                    <Link as={Link} to="/motorcycles/stock"><Card.Img variant="bottom" src={motorcycleStockImage} /></Link>
+                </Card>
+            </Col>
+        </Row>
+    );
+};
 
 export default Motorcycles;
