771356183513c91b50fafa7f79781035aca88992
Bárdos István <bardos.istvan@icloud.com>
2023-01-29 11:37:25 +0100

    Create table to show all manufacturers
    Table shown automatically when visiting Manufacturers page

--- src/main/ui/src/pages/Manufacturers.js
+++ src/main/ui/src/pages/Manufacturers.js
@@ -1,9 +1,66 @@
+import Table from "react-bootstrap";
+import useState from "react";
 const Manufacturers = () => {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [manufacturers, setManufacturers] = useState([]);
+
+    // Fetching data
+    useEffect(() => {
+        fetch("manufacturer/get/all")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setManufacturers(result);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }, [])
+
+    // Check errors and show loading status
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <>
             <h2 className="pageTitle">Manufacturers</h2>
+            <ManufacturersTable manufacturers={manufacturers} />
         </>
     )
 };
+
+const ManufacturersTable = (props) => {
+
+
+    return (
+        <Table striped bordered hover variant="dark">
+            <thead>
+            <tr>
+                <th>id</th>
+                <th>Name</th>
+                <th>Country</th>
+                <th>Partner since</th>
+            </tr>
+            </thead>
+            <tbody>
+            {props.manufacturers.map(m => (
+                <tr key={m.id}>
+                    <td>{m.id}</td>
+                    <td>{m.name}</td>
+                    <td>{m.country}</td>
+                    <td>{m.partnerSince}</td>
+                </tr>
+            ))}
+            </tbody>
+        </Table>
+    )
+}
 
 export default Manufacturers;
