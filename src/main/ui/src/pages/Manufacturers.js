e1c4371617e816cff67ef576cb8d63757c2c5630
Bárdos István <bardos.istvan@icloud.com>
2023-02-14 18:50:10 +0100

    Implement table Live search feature

--- src/main/ui/src/pages/Manufacturers.js
+++ src/main/ui/src/pages/Manufacturers.js
@@ -1,12 +1,18 @@
 import {useEffect, useState} from "react";
+
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import {FormControl} from "react-bootstrap";
+import Container from "react-bootstrap/Container";
 import StripedTable from "../components/shared/table/StripedTable";
 
 import {fetchData} from "../util/fetchData";
 
 
 const Manufacturers = () => {
-    const [error, setError] = useState(null);
+    // States for fetched data
     const [isLoaded, setIsLoaded] = useState(false);
+    const [error, setError] = useState(null);
     const [manufacturers, setManufacturers] = useState([]);
     const [filteredData, setFilteredData] = useState([]);
     const [updateModalShow, setUpdateModalShow] = useState(false);
@@ -14,12 +20,26 @@ const Manufacturers = () => {
     const [recordId, setRecordId] = useState("");
 
     // Fetching data
+
+    // Table live search feature
+    async function handleSearch(event) {
+        setFilteredData(manufacturers.filter((item) =>
+            item.id.toString().includes(event.target.value.toLowerCase()) ||
+            item.name.toLowerCase().includes(event.target.value.toLowerCase()) ||
+            item.country.toLowerCase().includes(event.target.value.toLowerCase()) ||
+            item.partnerSince.toString().includes(event.target.value.toLowerCase())
+        ));
+    }
+
+
+    // Fetching data for all the components of Manufacturers page
     useEffect(() => {
         fetchData("manufacturer/get/all")
             .then(
                 (result) => {
                     setIsLoaded(true);
                     setManufacturers(result);
+                    setFilteredData(result);
                 },
                 (error) => {
                     setIsLoaded(true);
@@ -28,20 +48,25 @@ const Manufacturers = () => {
             )
     }, [])
 
-    // Check errors and show loading status
+    // Check for errors during fetch data, and show loading status
     if (error) {
         return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
         return <div>Loading...</div>;
     }
 
+
     return (
         <>
-            <h2 className="pageTitle">Manufacturers</h2>
-            <ManufacturersTable manufacturers={manufacturers} />
-        </>
-    )
-};
+            <h2 className="page-title">Manufacturers</h2>
+            <Container id="search-and-add-bar">
+                <Row>
+                    <Col>
+                        <FormControl id="search-box" type="text" onChange={async (event) => await handleSearch(event)}
+                                     placeholder="Type here to filter..."/>
+                    </Col>
+                </Row>
+            </Container>
 
             <StripedTable originalData={manufacturers} filteredData={filteredData} setRecordId={setRecordId}
                           setUpdateModalShow={setUpdateModalShow} setDeleteModalShow={setDeleteModalShow}/>
