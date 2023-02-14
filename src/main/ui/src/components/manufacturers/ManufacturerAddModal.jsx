4795e8c85b82b5e68a2ad8f04e7f2bb78accd5c9
Bárdos István <bardos.istvan@icloud.com>
2023-02-14 18:57:54 +0100

    Implement Manufacturer add feature
    - Create ManufacturerAddModal.
    - Embed a button to Manufacturers page to open corresponding modal window.

---
+++ src/main/ui/src/components/manufacturers/ManufacturerAddModal.jsx
@@ -0,0 +1,85 @@
+import {useState} from "react";
+
+import {Form} from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+
+import CrudModal from "../shared/CrudModal";
+
+
+const ManufacturerAddModal = (props) => {
+    const modalBody = (<AddForm setFormSubmit={props.setFormSubmit} setAddModalShow={props.setAddModalShow} />);
+
+    return <CrudModal show={props.show} onHide={props.onHide} title="Add new manufacturer" body={modalBody} />
+}
+
+
+const AddForm = (props) => {
+    const [modalName, setModalName] = useState("");
+    const [modalCountry, setModalCountry] = useState("");
+    const [modalPartnerSince, setModalPartnerSince] = useState("");
+    const [incompleteFormAlert, setIncompleteFormAlert] = useState("");
+
+    const currentDate = new Date().toJSON().slice(0, 10);
+
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+
+        const url = "manufacturer/add";
+
+        const requestBody = {
+            "name": modalName,
+            "country": modalCountry,
+            "partnerSince": modalPartnerSince ? modalPartnerSince : currentDate
+        }
+
+        const options = {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(requestBody),
+        }
+
+        await fetch(url, options);
+        props.setFormSubmit(old => !old);
+    }
+
+
+    return (
+        <Form>
+            <Form.Group className="mb-3" controlId="formTextareaName">
+                <Form.Label>Manufacturer name<span style={{color: "red"}}>*</span></Form.Label>
+                <Form.Control type="text" name="name" placeholder="Name"
+                              onChange={(event) => setModalName(event.target.value)}/>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formTextareaCountry">
+                <Form.Label>Country of origin<span style={{color: "red"}}>*</span></Form.Label>
+                <Form.Control type="text" name="country" placeholder="Country"
+                              onChange={(event) => setModalCountry(event.target.value)}/>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formDatePartnerSince">
+                <Form.Label>Partner since</Form.Label>
+                <Form.Control type="text" onFocus={(event) => (event.target.type = "date")}
+                              onBlur={(event) => (event.target.type = "text")} defaultValue={currentDate}
+                              name="partnerSince" onChange={(event) => setModalPartnerSince(event.target.value)}/>
+            </Form.Group>
+
+            <p style={{fontWeight: "bold", textAlign: "center"}}>{incompleteFormAlert}</p>
+
+            <div style={{display: "flex", justifyContent: "center"}}>
+                <Button type="button" variant="secondary" style={{paddingInline: "30px"}}
+                        onClick={async (event) => {
+                            if (!modalName || !modalCountry) {
+                                setIncompleteFormAlert("Name and Country fields are both mandatory!");
+                            } else {
+                                await handleSubmit(event);
+                                props.setAddModalShow(false);
+                            }
+                        }}>Update</Button>
+            </div>
+        </Form>
+    )
+}
+
+export default ManufacturerAddModal;
--- src/main/ui/src/pages/Manufacturers.js
+++ src/main/ui/src/pages/Manufacturers.js
@@ -4,7 +4,10 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import {FormControl} from "react-bootstrap";
 import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+
 import StripedTable from "../components/shared/table/StripedTable";
+import ManufacturerAddModal from "../components/manufacturers/ManufacturerAddModal";
 
 import {fetchData} from "../util/fetchData";
 
@@ -15,11 +18,16 @@ const Manufacturers = () => {
     const [error, setError] = useState(null);
     const [manufacturers, setManufacturers] = useState([]);
     const [filteredData, setFilteredData] = useState([]);
+
+    // States for Modals
+    const [addModalShow, setAddModalShow] = useState(false);
     const [updateModalShow, setUpdateModalShow] = useState(false);
     const [deleteModalShow, setDeleteModalShow] = useState(false);
     const [recordId, setRecordId] = useState("");
 
-    // Fetching data
+    // State for table re-render
+    const [formSubmit, setFormSubmit] = useState(false);
+
 
     // Table live search feature
     async function handleSearch(event) {
@@ -45,8 +53,8 @@ const Manufacturers = () => {
                     setIsLoaded(true);
                     setError(error);
                 }
-            )
-    }, [])
+            );
+    }, [formSubmit])
 
     // Check for errors during fetch data, and show loading status
     if (error) {
@@ -65,6 +73,9 @@ const Manufacturers = () => {
                         <FormControl id="search-box" type="text" onChange={async (event) => await handleSearch(event)}
                                      placeholder="Type here to filter..."/>
                     </Col>
+                    <Col xs={2}>
+                        <Button variant="secondary" onClick={() => setAddModalShow(true)}>Add new item</Button>
+                    </Col>
                 </Row>
             </Container>
 
@@ -72,6 +83,10 @@ const Manufacturers = () => {
                           setUpdateModalShow={setUpdateModalShow} setDeleteModalShow={setDeleteModalShow}/>
 
 
+            <ManufacturerAddModal setFormSubmit={setFormSubmit} show={addModalShow} setAddModalShow={setAddModalShow}
+                                  onHide={() => setAddModalShow(false)}/>
+
+        </>
     )
 }
 
