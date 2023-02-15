22d8844a43800b04ccb1ce9145585646d4980499
Bárdos István <bardos.istvan@icloud.com>
2023-02-15 17:50:19 +0100

    Import new App component into index.jsx

--- src/main/ui/src/index.jsx
+++ src/main/ui/src/index.jsx
@@ -1,35 +1,9 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Layout from "./pages/Layout";
-import Home from "./pages/Home";
-import Manufacturers from "./pages/Manufacturers";
-import Motorcycles from "./pages/motorcycles/Motorcycles";
-import MotorcycleModel from "./pages/motorcycles/MotorcycleModel";
-import MotorcycleStock from "./pages/motorcycles/MotorcycleStock";
-import NoPage from "./pages/NoPage";
+import App from "./App";
 
 import "./index.css";
 
-
-export default function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Home />} />
-                    <Route path="manufacturers" element={<Manufacturers />} />
-                    <Route path="motorcycles">
-                        <Route index element={<Motorcycles />} />
-                        <Route path="model" element={<MotorcycleModel />} />
-                        <Route path="stock" element={<MotorcycleStock />} />
-                    </Route>
-                    <Route path="*" element={<NoPage />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    );
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
