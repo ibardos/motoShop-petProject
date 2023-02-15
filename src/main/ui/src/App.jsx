c34edbd82e529c2cab2b4e962f6cbc32cc0fded3
Bárdos István <bardos.istvan@icloud.com>
2023-02-15 17:49:53 +0100

    Create App.jsx
    Outsource the content of App() function into a new component for greater separation.

---
+++ src/main/ui/src/App.jsx
@@ -0,0 +1,30 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Manufacturers from "./pages/Manufacturers";
+import Motorcycles from "./pages/motorcycles/Motorcycles";
+import MotorcycleModel from "./pages/motorcycles/MotorcycleModel";
+import MotorcycleStock from "./pages/motorcycles/MotorcycleStock";
+import NoPage from "./pages/NoPage";
+
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="manufacturers" element={<Manufacturers />} />
+                    <Route path="motorcycles">
+                        <Route index element={<Motorcycles />} />
+                        <Route path="model" element={<MotorcycleModel />} />
+                        <Route path="stock" element={<MotorcycleStock />} />
+                    </Route>
+                    <Route path="*" element={<NoPage />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
