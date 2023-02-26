6926866c8e375695e6716278d8ac4e3a7841d9d9
Bárdos István <bardos.istvan@icloud.com>
2023-02-26 15:42:10 +0100

    Create ManufacturerControllerTest.java

---
+++ src/test/java/com/ibardos/motoShop/controller/ManufacturerControllerTest.java
@@ -0,0 +1,18 @@
+package com.ibardos.motoShop.controller;
+
+import com.ibardos.motoShop.util.JsonReader;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+import java.nio.file.Path;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class ManufacturerControllerTest {
+}
