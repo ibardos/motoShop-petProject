1aaab36afe8eef8ae169b187f0d581ec4c7a60ae
Bárdos István <bardos.istvan@icloud.com>
2023-02-26 17:37:39 +0100

    Create MotorcycleStockControllerTest.java

---
+++ src/test/java/com/ibardos/motoShop/controller/MotorcycleStockControllerTest.java
@@ -0,0 +1,16 @@
+package com.ibardos.motoShop.controller;
+
+import com.ibardos.motoShop.util.JsonReader;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.file.Path;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class MotorcycleStockControllerTest {
+}
