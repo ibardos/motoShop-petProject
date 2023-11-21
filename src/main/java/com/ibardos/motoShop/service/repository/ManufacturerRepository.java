46706936e2b7e600371a94231040c3794c9e6fdb
Bárdos István <bardos.istvan@icloud.com>
2023-11-21 15:24:47 +0100

    Create ManufacturerRepository.java

---
+++ src/main/java/com/ibardos/motoShop/service/repository/ManufacturerRepository.java
@@ -0,0 +1,13 @@
+package com.ibardos.motoShop.service.repository;
+
+import com.ibardos.motoShop.model.Manufacturer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * Spring Data JPA repository interface for managing entities of Manufacturer type in the database.
+ */
+public interface ManufacturerRepository extends JpaRepository<Manufacturer, Integer> {
+}
