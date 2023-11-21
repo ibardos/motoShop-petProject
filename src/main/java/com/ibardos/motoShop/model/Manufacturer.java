6798db3ab71538bdf72681a90d59fa4a624bdd0f
Bárdos István <bardos.istvan@icloud.com>
2023-11-21 15:24:28 +0100

    Annotate Manufacturer type to use as JPA Entity

--- src/main/java/com/ibardos/motoShop/model/Manufacturer.java
+++ src/main/java/com/ibardos/motoShop/model/Manufacturer.java
@@ -1,5 +1,10 @@
 package com.ibardos.motoShop.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
@@ -9,11 +14,14 @@ import java.sql.Date;
 /**
  * Model class, representing a Manufacturer in DB.
  */
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
 public class Manufacturer {
     // Properties
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String country;
