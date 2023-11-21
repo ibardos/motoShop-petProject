20fb70b35c385fb4ccaf24d597f1c9fff5b7d1aa
Bárdos István <bardos.istvan@icloud.com>
2023-11-21 15:23:59 +0100

    Update Manufacturer.java
    Remove boilerplate code with Lombok.

--- src/main/java/com/ibardos/motoShop/model/Manufacturer.java
+++ src/main/java/com/ibardos/motoShop/model/Manufacturer.java
@@ -1,44 +1,21 @@
 package com.ibardos.motoShop.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
 import java.sql.Date;
 
 /**
  * Model class, representing a Manufacturer in DB.
  */
+@Getter
+@Setter
+@NoArgsConstructor
 public class Manufacturer {
     // Properties
     private int id;
     private String name;
     private String country;
     private Date partnerSince;
-
-
-    // Constructor
-    public Manufacturer(String name, String country, Date partnerSince) {
-        this.name = name;
-        this.country = country;
-        this.partnerSince = partnerSince;
-    }
-
-
-    // Getters and setters
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public Date getPartnerSince() {
-        return partnerSince;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
 }
