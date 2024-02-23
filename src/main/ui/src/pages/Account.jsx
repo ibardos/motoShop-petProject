aa42acffa9ec2a41b2cdb31b27946c18732ba23f
Bárdos István <bardos.istvan@icloud.com>
2024-02-23 16:42:46 +0100

    Create new page components to previously defined new routes

---
+++ src/main/ui/src/pages/Account.jsx
@@ -0,0 +1,16 @@
+import {useContext} from "react";
+
+import {Image} from "react-bootstrap";
+
+
+const Account = () => {
+    return (
+        <>
+            <h2 className="page-title">My account</h2>
+            <Image style={{padding: "30px"}} src={underConstructionImage}/>
+        </>
+    )
+}
+
+
+export default Account;
---
+++ src/main/ui/src/pages/User.jsx
@@ -0,0 +1,16 @@
+import underConstructionImage from "../assets/img/error/underConstruction.jpg";
+
+import {Image} from "react-bootstrap";
+
+
+const User = () => {
+    return (
+        <>
+            <h2 className="page-title">Users</h2>
+            <Image src={underConstructionImage}/>
+        </>
+    )
+}
+
+
+export default User;
