e2a96baf2d39132314615c102909de8419e19800
Bárdos István <bardos.istvan@icloud.com>
2024-02-23 16:35:49 +0100

    Update Motorcycles.jsx
    Refactor routing.

--- src/main/ui/src/pages/motorcycles/Motorcycles.jsx
+++ src/main/ui/src/pages/motorcycles/Motorcycles.jsx
@@ -11,14 +11,14 @@ import motorcycleStockImage from "../../assets/img/motorcycles/motorcycleStock.j
 const Motorcycles = () => {
     return (
         <>
-            <h2 className="page-title">Motorcycles</h2>
-            <Services />
+            <h2 className="page-title">Motorcycle services</h2>
+            <MotorcycleServices/>
         </>
     )
 }
 
 
-const Services = () => {
+const MotorcycleServices = () => {
     return (
         <Row xs={1} md={2} className="g-3">
             <Col>
@@ -29,7 +29,7 @@ const Services = () => {
                             Create, read, update, delete Motorcycle models.
                         </Card.Text>
                     </Card.Body>
-                    <Link as={Link} to="/motorcycles/model"><Card.Img variant="bottom" src={motorcycleModelImage} /></Link>
+                    <Link as={Link} to="/service/motorcycle/model"><Card.Img variant="bottom" src={motorcycleModelImage} /></Link>
                 </Card>
             </Col>
 
@@ -38,10 +38,10 @@ const Services = () => {
                     <Card.Body>
                         <Card.Title>Motorcycle stock</Card.Title>
                         <Card.Text>
-                            Create, read, update, delete Motorcycle models in stock.
+                            Create, read, update, delete Motorcycle items in stock.
                         </Card.Text>
                     </Card.Body>
-                    <Link as={Link} to="/motorcycles/stock"><Card.Img variant="bottom" src={motorcycleStockImage} /></Link>
+                    <Link as={Link} to="/service/motorcycle/stock"><Card.Img variant="bottom" src={motorcycleStockImage} /></Link>
                 </Card>
             </Col>
         </Row>
