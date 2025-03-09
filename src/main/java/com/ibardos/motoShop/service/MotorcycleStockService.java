726eec1ab89390fa64408ae56b60af0aea12cdb8
Istvan Bardos <bardos.istvan@icloud.com>
2025-03-09 14:09:42 +0100

    Refactor setCalculatedFieldsOfMotorcycleStockObjectFromClient helper method
    Calculation of profitOnUnit has been refactored to be more concise, verbose, more performant and easy to follow.

--- src/main/java/com/ibardos/motoShop/service/MotorcycleStockService.java
+++ src/main/java/com/ibardos/motoShop/service/MotorcycleStockService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 import java.util.Optional;
 
@@ -80,7 +81,12 @@ public class MotorcycleStockService {
     private void setCalculatedFieldsOfMotorcycleStockObjectFromClient(MotorcycleStock motorcycleStock) {
         BigDecimal purchasingPrice = motorcycleStock.getPurchasingPrice();
         BigDecimal profitMargin = motorcycleStock.getProfitMargin();
-        BigDecimal profitOnUnit = BigDecimal.valueOf(Math.ceil(Double.parseDouble(String.valueOf(purchasingPrice.multiply(profitMargin)))/100)*100);
+
+        // Round up to the nearest hundred
+        BigDecimal profitOnUnit = purchasingPrice.multiply(profitMargin)
+                .divide(BigDecimal.valueOf(100), 0, RoundingMode.CEILING)
+                .multiply(BigDecimal.valueOf(100));
+
         BigDecimal sellingPrice = purchasingPrice.add(profitOnUnit);
 
         motorcycleStock.setProfitOnUnit(profitOnUnit);
