d66db21e125ef033ce20111ea785b322699dd072
Istvan Bardos <istvan.bardos@icloud.com>
2025-06-09 16:12:27 +0200

    Fix identifyMotorcycleModelObject function [CI]
    Object comparison from checking simple name contain, updated to check "re-constructed" names against names comping from drop-down. This modification fixes a bug, where motorcycle models were falsely identified when having the same model name, but from different manufacturers.

--- src/main/ui/src/util/identifyMotorcycleModelObject.js
+++ src/main/ui/src/util/identifyMotorcycleModelObject.js
@@ -1,18 +1,17 @@
 /**
- * Identifies an object of type motorcycleModel from a collection, based on a given String expression by checking the
- * expression against the names of the objects.
+ * Identifies an object of type motorcycleModel from a collection, based on a given String expression (manufacturer.name - model.name)
+ * by checking the expression against the "constructed" names of the objects.
  *
  * Dropdown (<option>) shows "manufacturer.name + motorcycleModel.name" to client, to enhance user experience,
- * therefore have to find the right motorcycleModel based on motorcycleModel.name only, among existing objects,
- * to be passed to back-end.
+ * therefore have to find the right motorcycleModel based on these String values only, among existing objects.
  *
  * @param motorcycleModelsCollection set of motorcycleModel objects currently available in DB
  * @param motorcycleModelNameFromDropdown that was chosen by the client as "manufacturer.name + motorcycleModel.name"
- * @returns object of type motorcycleModel if there is a match based on motorcycleModel.name, otherwise undefined
+ * @returns object of type motorcycleModel if there is a match, otherwise undefined
  */
 export function identifyMotorcycleModelObject(motorcycleModelsCollection, motorcycleModelNameFromDropdown) {
     for (const motorcycleModelObject of motorcycleModelsCollection) {
-        if (motorcycleModelNameFromDropdown.includes(motorcycleModelObject.modelName)) {
+        if (motorcycleModelNameFromDropdown === motorcycleModelObject.manufacturer.name + " - " + motorcycleModelObject.modelName) {
             return motorcycleModelObject;
         }
     }
